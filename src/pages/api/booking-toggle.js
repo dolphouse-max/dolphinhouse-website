91fad18e7c7eff354e657cd706eb7f73e50eef28
Girish <gjpatil@gmail.com>
2025-11-16 12:02:04 +0530

    Booking page: render paused state server-side using global toggle; add no-store Cache-Control on toggle API

--- src/pages/api/booking-toggle.js
+++ src/pages/api/booking-toggle.js
@@ -12,6 +12,7 @@ function json(data, status = 200) {
     status,
     headers: {
       'Content-Type': 'application/json',
+      'Cache-Control': 'no-store',
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'GET, PUT, OPTIONS',
       'Access-Control-Allow-Headers': 'Content-Type'
